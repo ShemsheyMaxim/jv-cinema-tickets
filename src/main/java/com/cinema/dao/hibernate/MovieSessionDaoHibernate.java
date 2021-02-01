4ba12f32c207b45a6e73ed711fb39e73be2fa2cb
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-01 20:53:22 +0200

    added interfaces CinemaHallDao and MovieSessionDao, added classes CinemaHallDaoHibernate and MovieSessionDaoHibernate

---
+++ src/main/java/com/cinema/dao/hibernate/MovieSessionDaoHibernate.java
@@ -0,0 +1,57 @@
+package com.cinema.dao.hibernate;
+
+import com.cinema.dao.MovieSessionDao;
+import com.cinema.exception.DataProcessingException;
+import com.cinema.lib.Dao;
+import com.cinema.model.MovieSession;
+import com.cinema.util.HibernateUtil;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoHibernate implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movieSession "
+                    + movieSession + " to table. ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> sessionsOfMoviesQuery =
+                    session.createQuery("SELECT ms FROM MovieSession ms "
+                            + "WHERE ms.movie.id = :movieId "
+                            + "AND DATE_FORMAT(ms.showTime, '%Y-%m-%d') "
+                            + "= :date", MovieSession.class);
+            sessionsOfMoviesQuery.setParameter("movieId", movieId);
+            sessionsOfMoviesQuery.setParameter("date",
+                    date.format(DateTimeFormatter.ISO_LOCAL_DATE));
+            return sessionsOfMoviesQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error retrieving "
+                    + "all available sessions of films. ", e);
+        }
+    }
+}
