873420bdc22ed78fd674976514a994ed07f6c328
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-03 01:22:53 +0200

    Added model User, added interface UserDao and class UserDaoHibernate

---
+++ src/main/java/com/cinema/dao/hibernate/UserDaoHibernate.java
@@ -0,0 +1,50 @@
+package com.cinema.dao.hibernate;
+
+import com.cinema.dao.UserDao;
+import com.cinema.exception.DataProcessingException;
+import com.cinema.lib.Dao;
+import com.cinema.model.User;
+import com.cinema.util.HibernateUtil;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class UserDaoHibernate implements UserDao {
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert user " + user + " to table. ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> findUserByEmailQuery =
+                    session.createQuery("FROM User WHERE User.email = :email", User.class);
+            findUserByEmailQuery.setParameter("email", User.class);
+            return findUserByEmailQuery.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find user for email "
+                    + email + " in table. ", e);
+        }
+    }
+}
