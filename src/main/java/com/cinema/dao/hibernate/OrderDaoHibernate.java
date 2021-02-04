79984523675012162d5d6d8ebab1e805a0f8509c
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-04 19:18:52 +0200

    Added interface OrderDao and class OrderDaoHibernate

---
+++ src/main/java/com/cinema/dao/hibernate/OrderDaoHibernate.java
@@ -0,0 +1,50 @@
+package com.cinema.dao.hibernate;
+
+import com.cinema.dao.OrderDao;
+import com.cinema.exception.DataProcessingException;
+import com.cinema.lib.Dao;
+import com.cinema.model.Order;
+import com.cinema.model.User;
+import com.cinema.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class OrderDaoHibernate implements OrderDao {
+
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert order " + order + " to table. ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> listOrderByUserQuery = session.createQuery("SELECT DISTINCT o "
+                    + "FROM Order o LEFT JOIN FETCH o.tickets WHERE o.user = :user", Order.class);
+            listOrderByUserQuery.setParameter("user", user);
+            return listOrderByUserQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get history orders for user " + user, e);
+        }
+    }
+}
