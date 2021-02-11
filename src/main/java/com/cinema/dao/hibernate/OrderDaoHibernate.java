d7580de3481abd8557e730d82ee754a3e5664f44
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-12 01:35:13 +0200

    rewrote dao classes dependencies from hibernate on spring.

--- src/main/java/com/cinema/dao/hibernate/OrderDaoHibernate.java
+++ src/main/java/com/cinema/dao/hibernate/OrderDaoHibernate.java
@@ -2,24 +2,31 @@ package com.cinema.dao.hibernate;
 
 import com.cinema.dao.OrderDao;
 import com.cinema.exception.DataProcessingException;
-import com.cinema.lib.Dao;
 import com.cinema.model.Order;
 import com.cinema.model.User;
-import com.cinema.util.HibernateUtil;
 import java.util.List;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-@Dao
+@Repository
 public class OrderDaoHibernate implements OrderDao {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public OrderDaoHibernate(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public Order add(Order order) {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             transaction = session.beginTransaction();
             session.save(order);
             transaction.commit();
@@ -38,7 +45,7 @@ public class OrderDaoHibernate implements OrderDao {
 
     @Override
     public List<Order> getOrdersHistory(User user) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             Query<Order> listOrderByUserQuery = session.createQuery("SELECT DISTINCT o "
                     + "FROM Order o LEFT JOIN FETCH o.tickets WHERE o.user = :user", Order.class);
             listOrderByUserQuery.setParameter("user", user);
