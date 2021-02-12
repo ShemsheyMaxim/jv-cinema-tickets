6c8c3badadc6f0e525e01f451c60351fd3a6454d
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-12 02:17:55 +0200

    added package controllers and class HelloController, deleted class Main

--- src/main/java/com/cinema/config/AppConfig.java
+++ src/main/java/com/cinema/config/AppConfig.java
@@ -1,12 +1,5 @@
 package com.cinema.config;
 
-import com.cinema.model.CinemaHall;
-import com.cinema.model.Movie;
-import com.cinema.model.MovieSession;
-import com.cinema.model.Order;
-import com.cinema.model.ShoppingCart;
-import com.cinema.model.Ticket;
-import com.cinema.model.User;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -47,17 +40,11 @@ public class AppConfig {
         LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
         localSessionFactoryBean.setDataSource(getDataSource());
         Properties properties = new Properties();
-        properties.put("hibernate.snow_sql", environment.getProperty("hibernate.snow_sql"));
+        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
         properties.put("hibernate.hdm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
 
         localSessionFactoryBean.setHibernateProperties(properties);
-        localSessionFactoryBean.setAnnotatedClasses(CinemaHall.class);
-        localSessionFactoryBean.setAnnotatedClasses(Movie.class);
-        localSessionFactoryBean.setAnnotatedClasses(MovieSession.class);
-        localSessionFactoryBean.setAnnotatedClasses(Order.class);
-        localSessionFactoryBean.setAnnotatedClasses(ShoppingCart.class);
-        localSessionFactoryBean.setAnnotatedClasses(Ticket.class);
-        localSessionFactoryBean.setAnnotatedClasses(User.class);
+        localSessionFactoryBean.setPackagesToScan("com.cinema.model");
         return localSessionFactoryBean;
     }
 }
