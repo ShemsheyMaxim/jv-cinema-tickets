0999135de90d2b7385ec80cb29535ae504fd2eec
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-15 00:02:27 +0200

    added classes MovieRequestDto and MovieResponseDto, added interface MovieMapper and class MovieMapperImpl.

---
+++ src/main/java/com/cinema/service/mapper/impl/MovieMapperImpl.java
@@ -0,0 +1,25 @@
+package com.cinema.service.mapper.impl;
+
+import com.cinema.model.Movie;
+import com.cinema.model.dto.MovieRequestDto;
+import com.cinema.model.dto.MovieResponseDto;
+import com.cinema.service.mapper.MovieMapper;
+
+public class MovieMapperImpl implements MovieMapper {
+    @Override
+    public MovieResponseDto toMovieDto(Movie movie) {
+        MovieResponseDto responseDto = new MovieResponseDto();
+        responseDto.setId(movie.getId());
+        responseDto.setDescription(movie.getDescription());
+        responseDto.setTitle(movie.getTitle());
+        return responseDto;
+    }
+
+    @Override
+    public Movie toMovieEntity(MovieRequestDto movieRequestDto) {
+        Movie movie = new Movie();
+        movie.setTitle(movieRequestDto.getTitle());
+        movie.setDescription(movieRequestDto.getDescription());
+        return movie;
+    }
+}
