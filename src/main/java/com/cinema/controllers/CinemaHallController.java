2de636a753980e64baa1bd00ce1d7db9fed4969c
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-15 02:43:31 +0200

    added classes CinemaHallController and MovieController.

---
+++ src/main/java/com/cinema/controllers/CinemaHallController.java
@@ -0,0 +1,43 @@
+package com.cinema.controllers;
+
+import com.cinema.model.CinemaHall;
+import com.cinema.model.dto.CinemaHallRequestDto;
+import com.cinema.model.dto.CinemaHallResponseDto;
+import com.cinema.service.CinemaHallService;
+import com.cinema.service.mapper.CinemaHallMapper;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/cinema-halls")
+public class CinemaHallController {
+    private final CinemaHallMapper cinemaHallMapper;
+    private final CinemaHallService cinemaHallService;
+
+    @Autowired
+    public CinemaHallController(CinemaHallMapper cinemaHallMapper,
+                                CinemaHallService cinemaHallService) {
+        this.cinemaHallMapper = cinemaHallMapper;
+        this.cinemaHallService = cinemaHallService;
+    }
+
+    @PostMapping
+    public void create(@RequestBody CinemaHallRequestDto cinemaHallRequestDto) {
+        CinemaHall cinemaHall = cinemaHallMapper.toCinemaHallEntity(cinemaHallRequestDto);
+        cinemaHallService.add(cinemaHall);
+    }
+
+    @GetMapping
+    public List<CinemaHallResponseDto> getAll() {
+        return cinemaHallService.getAll()
+                .stream()
+                .map(cinemaHallMapper::toCinemaHallDto)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/cinema/controllers/MovieController.java
@@ -0,0 +1,40 @@
+package com.cinema.controllers;
+
+import com.cinema.model.Movie;
+import com.cinema.model.dto.MovieRequestDto;
+import com.cinema.model.dto.MovieResponseDto;
+import com.cinema.service.MovieService;
+import com.cinema.service.mapper.MovieMapper;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieMapper movieMapper;
+    private final MovieService movieService;
+
+    public MovieController(MovieMapper movieMapper, MovieService movieService) {
+        this.movieMapper = movieMapper;
+        this.movieService = movieService;
+    }
+
+    @PostMapping
+    public void create(@RequestBody MovieRequestDto movieRequestDto) {
+        Movie movie = movieMapper.toMovieEntity(movieRequestDto);
+        movieService.add(movie);
+    }
+
+    @GetMapping
+    public List<MovieResponseDto> getAll() {
+        return movieService.getAll()
+                .stream()
+                .map(movieMapper::toMovieDto)
+                .collect(Collectors.toList());
+    }
+}
