a0fd30db2a37d144bd34cad3085772f279b0ddb4
ShemsheyMaxim <shanhaymax@gmail.com>
2021-02-19 00:20:19 +0200

    added class CustomGlobalExceptionHandler, deleted class HashUtil, deleted method login() from interface AuthenticationService and class AuthenticationServiceImpl, changed salt on encoder in class UserServiceImpl in method add()

--- src/main/java/com/cinema/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/cinema/service/impl/AuthenticationServiceImpl.java
@@ -1,12 +1,9 @@
 package com.cinema.service.impl;
 
-import com.cinema.exception.AuthenticationException;
 import com.cinema.model.User;
 import com.cinema.service.AuthenticationService;
 import com.cinema.service.ShoppingCartService;
 import com.cinema.service.UserService;
-import com.cinema.util.HashUtil;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,19 +17,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                                      ShoppingCartService shoppingCartService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
-    }
-
-    @Override
-    public User login(String email, String password) throws AuthenticationException {
-        Optional<User> foundUser = userService.findByEmail(email);
-        if (foundUser.isPresent()) {
-            User user = foundUser.get();
-            String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
-            if (user.getPassword().equals(hashedPassword)) {
-                return user;
-            }
-        }
-        throw new AuthenticationException("Can't find user with this email or password");
     }
 
     @Override
